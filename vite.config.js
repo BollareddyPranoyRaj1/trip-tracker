a648fdbfb38f7d16c8b8b90eeb4c1cb591fa1392
Bollareddy Pranoy Raj <bollareddypranoyraj@gmail.com>
2025-12-30 23:22:12 +0530

    Initial commit for fresh rebuild

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // Add this line to fix the blank screen on GitHub Pages
+  base: '/trip-tracker/', 
 })
